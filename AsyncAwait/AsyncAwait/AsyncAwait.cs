44514e9f5df0d85a06662144f9832a3a156e80bf
Dávid Kalácska <david.kalacska@dealogic.com>
2019-01-21 08:22:39 +0100

    first

---
+++ AsyncAwait/AsyncAwait/AsyncAwait.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class AsyncAwait
+    {
+        internal void Test()
+        {
+            DoItAsync();
+            Console.WriteLine(DoItAsyncWithTask().Result);
+            Console.WriteLine("DoItWithTask Finished");
+        }
+
+        private async void DoItAsync()
+        {
+            Console.WriteLine("DOIT Started");
+            Thread.Sleep(1000);
+            Console.WriteLine("DOIT Finished");
+        }
+
+        private async Task<int> DoItAsyncWithTask()
+        {
+            Console.WriteLine("DoItWithTask Started");
+            Thread.Sleep(2000);
+            return 10;
+        }
+
+    }
+}
---
+++ AsyncAwait/AsyncAwait/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test started");
+
+            AsyncAwait asyncAwait = new AsyncAwait();
+            Task.Run(()=>asyncAwait.Test());
+
+            Console.WriteLine("Test finished");
+            Console.ReadKey();
+        }
+    }
+}
